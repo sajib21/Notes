90f48ae21541b1fef5ec9e805e6a001cd2448ff5
sajib21 <shariarsajib@gmail.com>
2020-06-29 18:42:10 +0600

    Initial commit

---
+++ app/src/main/java/com/example/notes/Note.java
@@ -0,0 +1,27 @@
+package com.example.notes;
+
+public class Note {
+    private String title = "";
+    private String id = "";
+
+    public Note(String title, String id) {
+        this.title = title;
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
